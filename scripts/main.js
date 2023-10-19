c6af6d8e9521f4ce4bb1f201ac8eca59a338ea57
Densell Peters III <densell321@gmail.com>
2023-10-19 13:44:25 -0700

    add: freeze custom command

--- scripts/main.js
+++ scripts/main.js
@@ -11,21 +11,23 @@ console.warn("Script is running, do not panic!")
 
 // init code
 const world = server.world;
+const system = server.system;
 
 // Http Server
-let runServer = 1; // Set to 0 to disable feature
+let runServer = 0; // Set to 0 to disable feature
 
 //call main()
 main();
 
 async function main(){
 
-    // This whole block of code does not work right now
-    let client = await new net.HttpRequest("http://127.0.0.1:5000/server"); // Flask Server Address
-    let httpResp;
-    console.warn("Http Client has started");
-
-    await waitForRequest(runServer, client, httpResp); // wait for request from Server
+    if (runServer){
+        console.warn("Http Client has started");
+        // This whole block of code does not work right now
+        let client = await new net.HttpRequest("http://127.0.0.1:5000/server"); // Flask Server Address
+        let httpResp;
+        await waitForRequest(runServer, client, httpResp); // wait for request from Server
+    }
 }
 
 function getPlayers() {
@@ -33,6 +35,20 @@ function getPlayers() {
     const players = server.world.getPlayers();
     console.log(players); // Show us the player list
     return players;
+}
+
+function playerLookup(playerObj, name){
+    const size = playerObj.length
+    // console.warn(`list size is: ${size}`);
+    for (let i = 0; i < size; i++) {
+        // Code to be repeated goes here
+        // console.warn(`player ${playerObj[i].name} is in the list`);
+        if (playerObj[i].name === name){
+            // console.warn(`player ${name} exist so code can execute`);
+            return;
+        }
+      }
+      console.warn(`player ${name} does not exist, please try again`);
 }
 
 // async function setHttpData(data) {
@@ -154,3 +170,79 @@ world.afterEvents.itemUse.subscribe(async result => {
     }
 
 });
+
+world.afterEvents.chatSend.subscribe(result => {
+
+    const player = result.sender;
+    const msg = result.message;
+    result.cancel = true;
+    const players = getPlayers();
+
+    const msgList = msg.split(" ");
+
+    if (msgList[0] == "!freeze") {
+        if (msgList[0] == null || msgList[0] == ""){
+            console.warn("Player name not detected, please enter name");
+            return;
+        }
+        player.runCommand(`say the freeze command does not work yet :(`);
+        playerLookup(players, msgList[1]);
+    }
+});
+
+world.beforeEvents.chatSend.subscribe((data) => {
+    const player = data.sender;
+    const msg = data.message
+    const commands = [".hub", ".gmsp", ".help", ".gmc", ".arena", ".code", ".newskygen", ".gma", ".gui", ".staffcmds"]
+   system.run(()=>{
+    if (msg == commands[0]) {
+        data.cancel = true;
+        player.runCommandAsync('tp @s[scores={commandacess=!2}] 375 -59 78');
+        player.runCommandAsync('playsound random.orb @s[scores={commandacess=!2}]');
+        player.runCommandAsync('tellraw @s[scores={commandacess=!2}] {"rawtext":[{"text":"§aWarped to Hub."}]}');
+    } else if (msg == commands[1]) {
+        data.cancel = true;
+        player.runCommandAsync('gamemode spectator @s[scores={commandacess=!2..},tag=Owner,tag=Staff]')
+        player.runCommandAsync('playsound mob.blaze.shoot @s[scores={commandacess=!2..},tag=Owner,tag=Staff]')
+        player.runCommandAsync('tellraw @s[scores={commandacess=!2..},tag=Owner,tag=Staff] {"rawtext":[{"text":"§bSpectator has been enabled"}]}')
+    } else if (msg == commands[2]) {
+        data.cancel = true;
+        player.runCommandAsync('tellraw @s {"rawtext":[{"text":"§7.hub: Teleports you to the Hub in a matter of seconds, it cannot be used in certain places tho.\n§7.help: Provides a list of commands that you can use."}]}')
+    } else if (msg == commands[3]) {
+        data.cancel = true;
+        player.runCommandAsync('gamemode c @s[tag=Staff]')
+        player.runCommandAsync('tellraw @s[tag=Staff] {"rawtext":[{"text":"Gamemode set to Creative."}]}')
+    } else if (msg == commands[4]) {
+        data.cancel = true;
+        player.runCommandAsync('tp @s[tag=Staff] 1135 -61 -1132')
+        player.runCommandAsync('tellraw @s[tag=Staff] {"rawtext":[{"text":"§eUse .hub to teleport back to the hub."}]}')
+    } else if (msg == commands[5]) {
+        data.cancel = true;
+        player.sendMessage('§aClose chat to open the ui!')
+        system.run
+            (form.show(player))
+    }
+    else if (msg == commands[6]) {
+        data.cancel = true;
+        player.runCommandAsync('tp @s[tag=Staff] -21 282 -203')
+    }
+    else if (msg === commands[7]) {
+        data.cancel = true;
+        player.runCommandAsync('gamemode a @s[tag=Staff]')
+        player.runCommandAsync('tellraw @s[tag=Staff] {"rawtext":[{"text":"Gamemode set to Adventure."}]}')
+    }
+    else if (msg === commands[8]) {
+        data.cancel = true;
+        player.runCommandAsync('execute at @s run structure load gui ~~~')
+    }
+    else if (msg === commands[9]) {
+        data.cancel = true;
+        player.runCommandAsync('tellraw @s[tag=Staff] {"rawtext":[{"text":"§7.gmsp: Puts you in Spectator\n.gmc: Sets your Gamemode to Creative\n.arena: Teleports you to the Arena\n.newskygen: Teleports you to the new Skygen\n.gma: Sets your Gamemode to Adventure\n.gui: Gives you the WIP server GUI\n.staffcmds: Provides you a list of Staff only commands."}]}')
+    }
+    else if (!msg.startsWith(".")) {
+        return;
+    } else if (msg.startsWith(".") && msg != commands.length) {
+        data.cancel = true;
+        player.sendMessage("§cPlease provide a valid command.")
+    }})
+})
